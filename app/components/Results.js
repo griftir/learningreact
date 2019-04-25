9278176bb842b01222766f622886fed3c31803a8
v <magnowv@gmail.com>
2019-04-25 14:08:40 -0400

    more es6 refactoring

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,47 +1,49 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var queryString = require('query-string');
-var api = require('../utils/api');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
-var Loading = require('./Loading');
+const React = require("react");
+const PropTypes = require("prop-types");
+const queryString = require("query-string");
+const api = require("../utils/api");
+const Link = require("react-router-dom").Link;
+const PlayerPreview = require("./PlayerPreview");
+const Loading = require("./Loading");
 
-
-function Profile(props) {
-  var info = props.info;
+function Profile({ info }) {
   return (
     <PlayerPreview avatar={info.avatar_url} username={info.login}>
-      <ul className='space-list-items'>
+      <ul className="space-list-items">
         {info.name && <li>{info.name}</li>}
         {info.location && <li>{info.location}</li>}
         {info.company && <li>{info.company}</li>}
         <li>Followers: {info.followers}</li>
         <li>Following: {info.following}</li>
         <li>Public Repos: {info.public_repos}</li>
-        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+        {info.blog && (
+          <li>
+            <a href={info.blog}>{info.blog}</a>
+          </li>
+        )}
       </ul>
     </PlayerPreview>
-  )
+  );
 }
 Profile.propTypes = {
-  info: PropTypes.object.isRequired,
-}
+  info: PropTypes.object.isRequired
+};
 
-function Player(props) {
+function Player({ label, score, profile }) {
   return (
     <div>
-      <h1 className='header'>{props.label}</h1>
-      <h3 style={{ textAlign: 'center' }}>Score:{props.score}</h3>
-      <Profile info={props.profile} />
+      <h1 className="header">{label}</h1>
+      <h3 style={{ textAlign: "center" }}>Score:{score}</h3>
+      <Profile info={profile} />
     </div>
-  )
+  );
 }
 
 Player.propTypes = {
   label: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
-  profile: PropTypes.object.isRequired,
-}
+  profile: PropTypes.object.isRequired
+};
 
 class Results extends React.Component {
   constructor(props) {
@@ -52,72 +54,51 @@ class Results extends React.Component {
       loser: null,
       error: null,
       loading: true
-
-    }
+    };
   }
   componentDidMount() {
-    var players = queryString.parse(this.props.location.search);
+    const { playerOneName, playerTwoName } = queryString.parse(
+      this.props.location.search
+    );
 
-
-    api.battle([
-      players.playerOneName,
-      players.playerTwoName
-    ]).then(function (results) {
+    api.battle([playerOneName, playerTwoName]).then(results => {
       if (results === null) {
-        return this.setState(function () {
-          return {
-            error: 'Looks like there was an error. Check that bother users exist on github',
-            loading: false,
-          }
-        });
-      }
-      this.setState(function () {
-        return {
-          error: null,
-          winner: results[0],
-          loser: results[1],
+        return this.setState(() => ({
+          error:
+            "Looks like there was an error. Check that both users exist on github",
           loading: false
-
-        }
-      });
-
-    }.bind(this));
+        }));
+      }
+      this.setState(() => ({
+        error: null,
+        winner: results[0],
+        loser: results[1],
+        loading: false
+      }));
+    });
   }
   render() {
-    var error = this.state.error;
-    var winner = this.state.winner;
-    var loser = this.state.loser;
-    var loading = this.state.loading;
+    const { error, winner, loser, loading } = this.state;
 
     if (loading === true) {
-      return <Loading/>
+      return <Loading />;
     }
 
     if (error) {
       return (
         <div>
           <p>{error}</p>
-          <Link to='/battle'>Reset</Link>
+          <Link to="/battle">Reset</Link>
         </div>
-      )
+      );
     }
 
     return (
-      <div className='row'>
-        <Player
-          label='Winner'
-          score={winner.score}
-          profile={winner.profile}
-        />
-        <Player
-          label='Loser'
-          score={loser.score}
-          profile={loser.profile}
-        />
-
-
+      <div className="row">
+        <Player label="Winner" score={winner.score} profile={winner.profile} />
+        <Player label="Loser" score={loser.score} profile={loser.profile} />
       </div>
-    )
+    );
   }
 }
 
